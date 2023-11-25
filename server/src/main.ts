5ee0b666c512ecc0495c2a1c0a9f138f43c42bf6
hua_bang <47221942+hua-bang@users.noreply.github.com>
2023-11-25 08:55:56 +0800

    feat: 生成注释

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'; // Importing NestFactory from the core of NestJS
+import { AppModule } from './app.module'; // Importing the main app module
 
+// This is the bootstrap function that sets up the NestJS application
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule); // Creating a new NestJS application instance
+  await app.listen(3000); // The application starts listening for incoming requests on port 3000
 }
-bootstrap();
+bootstrap(); // Calling the bootstrap function to start the application
