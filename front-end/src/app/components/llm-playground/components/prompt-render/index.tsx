8baf23916e492e7cb3ac1b9e395784293b3b33e8
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-01-20 14:43:48 +0800

    feat: 更新用户头像

--- front-end/src/app/components/llm-playground/components/prompt-render/index.tsx
+++ front-end/src/app/components/llm-playground/components/prompt-render/index.tsx
@@ -14,8 +14,6 @@ const PromptRender: React.FC<PromptRenderProps> = (props) => {
       return undefined;
     }
 
-    console.log(content);
-
     const res = content.reduce((acc, curr) => {
       if (curr.type === "image_url" && curr.image_url) {
         acc.push(curr.image_url.url);
@@ -43,17 +41,30 @@ const PromptRender: React.FC<PromptRenderProps> = (props) => {
   return (
     <div className={styles.container}>
       <div className={styles.containerImg}>
-        <Image
-          style={{ borderRadius: "50%" }}
-          alt="logo"
-          preview={false}
-          width={40}
-          src={
-            prompt.role === "user"
-              ? "https://files.oaiusercontent.com/file-bqXaZBRh0ULv6TCCex5kY5d0?se=2123-10-19T07%3A59%3A01Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D4c4bd46d-e8d8-42ec-996b-87e3d2932009.png&sig=GaskdBJbUeQeVM7oNGRzqvcSzlRWYimX8aNjicdpYlE%3D"
-              : "https://files.oaiusercontent.com/file-NApY0rCxRGeqNC3z1Ha33WG0?se=2123-10-17T02%3A03%3A58Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Da15730bf-2048-47f4-b826-e02a67e31788.png&sig=FgYSAua12finxV6tdKL9BTJaDtHChokO77KKdMbI30U%3D"
-          }
-        />
+        {prompt.role === "user" ? (
+          <div
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: "50%",
+              lineHeight: "40px",
+              textAlign: "center",
+              border: "1px solid #ccc",
+            }}
+          >
+            🧑‍💻
+          </div>
+        ) : (
+          <Image
+            style={{ borderRadius: "50%" }}
+            alt="logo"
+            preview={false}
+            width={40}
+            src={
+              "https://files.oaiusercontent.com/file-NApY0rCxRGeqNC3z1Ha33WG0?se=2123-10-17T02%3A03%3A58Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Da15730bf-2048-47f4-b826-e02a67e31788.png&sig=FgYSAua12finxV6tdKL9BTJaDtHChokO77KKdMbI30U%3D"
+            }
+          />
+        )}
       </div>
       <div>
         <div className={styles.promptLabel}>
