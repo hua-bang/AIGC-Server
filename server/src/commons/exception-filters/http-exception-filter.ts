b5f094ffca505605cc02f81e63c447f0542293f7
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-01-21 19:15:00 +0800

    feat: update err message

--- server/src/commons/exception-filters/http-exception-filter.ts
+++ server/src/commons/exception-filters/http-exception-filter.ts
@@ -16,13 +16,19 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     let code = 500;
 
+    let message = '服务器异常';
+
     if (isObject(exceptionResponse)) {
       code = exceptionResponse.code || code;
     }
 
+    if (typeof exceptionResponse === 'string') {
+      message = exceptionResponse;
+    }
+
     response.status(status).json({
       code, // 使用异常中的 code
-      message: '服务器异常', // 使用异常中的消息
+      message, // 使用异常中的消息
       data: null, // 异常情况下，数据通常为空
     });
   }
