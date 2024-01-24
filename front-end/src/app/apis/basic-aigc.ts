2f381d84c06bb15d4fbbcb2a2195450a845bf5c6
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-01-24 21:00:44 +0800

    ✨ feat(fe): update get env

--- front-end/src/app/apis/basic-aigc.ts
+++ front-end/src/app/apis/basic-aigc.ts
@@ -1,29 +1,37 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { ChatParams } from "../apis-typings/basic-aigc";
 import { ChatType } from "../typings/llm";
 import { getStoreAppSetting } from "../hooks/use-setting/helper";
 
-console.log("process.env", process.env);
+let axiosInstance: AxiosInstance | undefined;
 
-const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACK_END_BASE_PATH,
-});
+const createAxiosInstance = () => {
+  console.log("process.env", process.env);
 
-axiosInstance.interceptors.response.use((res) => {
-  if (res.data.code !== 0) {
-    throw new Error(res.data.message);
-  }
+  axiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACK_END_BASE_PATH,
+  });
 
-  return res;
-});
+  axiosInstance.interceptors.response.use((res) => {
+    if (res.data.code !== 0) {
+      throw new Error(res.data.message);
+    }
+
+    return res;
+  });
+};
 
 export const getAIChat = (params: ChatParams) => {
+  if (!axiosInstance) {
+    createAxiosInstance();
+  }
+
   const url =
     params.chatType === ChatType.Vision
       ? "/basic-aigc/chatWithVision"
       : "/basic-aigc/chat";
 
-  return axiosInstance.post(url, params, {
+  return axiosInstance?.post(url, params, {
     headers: {
       "Content-Type": "application/json",
       OPENAI_API_KEY: getStoreAppSetting()?.OPENAI_API_KEY,
