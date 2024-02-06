7d16c8102ddc557ca442da81c0ec952f5e3bdab9
hua_bang <huabang001124@gmail.com>
2024-02-06 06:26:58 +0000

    feat: update server port

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
