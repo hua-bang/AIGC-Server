e327d5dce7b3746e069b80ee931bbcef060c961b
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-02-18 14:34:43 +0800

    ✨ feat: add menu icon

--- front-end/src/app/hooks/use-layout/index.tsx
+++ front-end/src/app/hooks/use-layout/index.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode, useState } from "react";
 import styles from "./index.module.scss";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { getIsMobile } from "@/app/utils/mobile";
 
 const useLayout = (options: useLayoutOptions) => {
   const [collapsed, setCollapsed] = useState(
@@ -10,17 +13,33 @@ const useLayout = (options: useLayoutOptions) => {
 
   const renderLayout = () => {
     return (
-      <div className={styles.layoutWrapper}>
-        <div
-          className={styles.leftContent}
-          style={{
-            width: collapsed ? undefined : "0px",
-          }}
-        >
-          {leftContent}
+      <>
+        <div className={styles.layoutWrapper}>
+          <div
+            className={styles.leftContent}
+            style={{
+              width: collapsed ? undefined : "0px",
+            }}
+          >
+            {leftContent}
+          </div>
+          <div className={styles.rightContent}>{rightContent}</div>
         </div>
-        <div className={styles.rightContent}>{rightContent}</div>
-      </div>
+        {getIsMobile() && (
+          <div className="fixed right-4 bottom-4">
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded"
+              onClick={() => {
+                setCollapsed(true);
+              }}
+            >
+              <Menu className="h-4 w-4" />
+            </Button>
+          </div>
+        )}
+      </>
     );
   };
 
